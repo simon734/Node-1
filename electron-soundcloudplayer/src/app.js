2df788f3f09418419915c400a7a57e0d9e103ec2
Cao Hong Phuoc <katanhich@gmail.com>
2017-06-28 01:27:06 +0700

    update

---
+++ electron-soundcloudplayer/src/app.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import ProgressSoundPlayer from './components/ProgressSoundPlayer';
+import SC from 'node-soundcloud';
+import Loading from 'react-loading';
+
+var client_id = 'YOUR SOUNDCLOUD APP ID';
